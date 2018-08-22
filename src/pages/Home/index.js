d770509819b2c1a073759a6bd09640c5b0fb3007
wangshijun <shijun@arcblock.io>
2018-08-22 15:05:53 +0800

    tweak home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -18,8 +18,16 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.welcome}>OCAP React Native Starter</Text>
+
+        <Text style={styles.header}>Libraries Integrated</Text>
+        <Text style={styles.item}>react-navigation</Text>
+        <Text style={styles.item}>react-native-vector-icons</Text>
+
+        <Text style={styles.header}>SDK Documentation</Text>
+        <Text style={styles.item}>github.com/Arcblock/ocap-javascript-sdk</Text>
+
+        <Text style={styles.header}>Happy Coding!</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
@@ -30,17 +38,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginTop: 30,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    marginBottom: 10,
+  },
+  header: {
+    color: '#000',
+    fontWeight: 'bold',
+    marginTop: 30,
+    marginBottom: 5,
+  },
+  item: {
+    color: '#333',
+    marginBottom: 5,
   },
   instructions: {
-    textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
